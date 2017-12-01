14d08d9f0f299be0a9981ffd70d8843f85335e74
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-01 09:57:32 -0600

    finish setup function

--- src/main/java/SentimentAnalysis.java
+++ src/main/java/SentimentAnalysis.java
@@ -17,20 +17,55 @@ import java.util.Map;
 
 public class SentimentAnalysis {
 
+    //inner class
     public static class SentimentSplit extends Mapper<Object, Text, Text, IntWritable> {
 
         public Map<String, String> emotionLibrary = new HashMap<String, String>();
 
+        @Override
+        public void setup(Context context) throws IOException {
+            //create emotionLibrary
+            //通过getConfiguration，动态的找到emotionCategory.txt的路径
+            Configuration configuration = context.getConfiguration();
+            String path = configuration.get("dictionary");  //不可以写死，让user自己define，让user从命令行传入
+
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line = br.readLine();
+
+            while (line != null) {
+                //line = "aquatic   neutral"
+                String[] word_feeling = line.split("\t");
+                emotionLibrary.put(word_feeling[0], word_feeling[1]);
+                line = br.readLine(); //read next line
+            }
+        }
 
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            //key 是offset:第几篇中的第几行 -> 位置
+            //value 是文章中的每一行 = line
 
+
+
+
+            //read file
+            //split into single words
+            //look up sentimentLibrary -> sentiment
+            //write out key value
+            String[] words = value.toString().split("\\s+"); //split by space
+            for (String word : words) {
+                if (emotionLibrary.containsKey(word.toLowerCase())) { //具有情感的单词
+                    String sentiment = emotionLibrary.get(word.toLowerCase());
+                    context.write(new Text(sentiment), new IntWritable(1));
+                }
+            }
         }
     }
 
     public static class SentimentCollection extends Reducer<Text, IntWritable, Text, IntWritable> {
 
         @Override
+        //override reduce方法
         public void reduce(Text key, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
 
